eba3ce31305495c2f44fb3a0d1fef7d8b3aa0ed9
Jean <jcharaco@unsa.edu.pe>
2023-07-19 17:08:58 -0500

    avance creacion de GUI

--- TriePrueba.java
+++ TriePrueba.java
@@ -1,7 +1,24 @@
+import java.awt.event.*;
 
-public class TriePrueba{
+import javax.swing.*;
+
+public class TriePrueba extends JFrame implements ActionListener{
 	public static void main(String [] args) {
-
+		// sujeto a cambios
+		new TriePrueba();
+	}
+	public TriePrueba() {// sujeto a cambios
+		setTitle("Operaciones trie");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
