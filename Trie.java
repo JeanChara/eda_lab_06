a80b491659e58761a47dac102ddca1a2471fea23
Joel Isaias Condori Leon <jcondorile@unsa.edu.pe>
2023-07-19 18:09:23 -0400

    Metodo reemplazar

--- Trie.java
+++ Trie.java
@@ -43,4 +43,22 @@ public class Trie {
         }
         return true;
     }
+
+    public String reemplazar(String text) {
+            
+        StringBuilder result = new StringBuilder();
+        List<String> words = Arrays.asList(text.split("\\s+")); // Dividir el texto en palabras
+
+        for (String word : words) {
+            if (search(word)) { // Verificar si la palabra está en el Trie
+                                           // Reemplazar la palabra por la deseada
+                result.append("REEMPLAZO"); // Reemplazar "REEMPLAZO" con la palabra deseada
+            } else {
+                result.append(word);
+            }
+            result.append(" ");
+        }
+
+        return result.toString().trim(); // Eliminar el espacio en blanco final y devolver el texto resultante
+    }
 }
