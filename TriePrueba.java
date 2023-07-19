b7f95b2b203f95231940e0b1991cbed25899e0cc
Jean <jcharaco@unsa.edu.pe>
2023-07-19 17:17:00 -0500

    creacion de metodo createContents y adicion de botones para las Operaciones

--- TriePrueba.java
+++ TriePrueba.java
@@ -1,8 +1,13 @@
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 import javax.swing.*;
 
 public class TriePrueba extends JFrame implements ActionListener{
+	private JLabel operaciones;
+	private JPanel panel1;
+	private JButton enviar,operacionInsertar,operacionBuscar,operacionReemplazar;
 	public static void main(String [] args) {
 		// sujeto a cambios
 		new TriePrueba();
@@ -10,11 +15,26 @@ public class TriePrueba extends JFrame implements ActionListener{
 	public TriePrueba() {// sujeto a cambios
 		setTitle("Operaciones trie");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(900,720);
 		setLocationRelativeTo(null);
+		
+		
+		createContents();
 		setVisible(true);
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
 	}
+	public void createContents() {
+		panel1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		operaciones = new JLabel("Operaciones");
+		operacionInsertar = new JButton("Insertar elemento");
+		operacionBuscar = new JButton("Buscar elemento");
+		operacionReemplazar = new JButton("Reemplazar elemento");
+		panel1.add(operaciones);
+		panel1.add(operacionInsertar);
+		panel1.add(operacionBuscar);
+		panel1.add(operacionReemplazar);
+		add(panel1);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
